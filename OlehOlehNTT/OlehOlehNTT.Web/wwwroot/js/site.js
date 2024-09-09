a3027d4ede600024fe6fea554c86da01a5082779
Lavitzz <allgaming1185@gmail.com>
2024-09-09 11:06:57 +0800

    Buat dari fernand

---
+++ OlehOlehNTT/OlehOlehNTT.Web/wwwroot/js/site.js
@@ -0,0 +1,98 @@
+﻿const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+
+const scrollRevealOption = {
+    origin: "bottom",
+    distance: "50px",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+ScrollReveal().reveal(".header__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".header__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+ScrollReveal().reveal(".header__btns", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+ScrollReveal().reveal(".service__content .section__subheader", {
+    ...scrollRevealOption,
+});
+ScrollReveal().reveal(".service__content .section__header", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+ScrollReveal().reveal(".service__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+ScrollReveal().reveal(".service__btn", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".service__card", {
+    duration: 1000,
+    interval: 500,
+    delay: 2000,
+});
+
+const swiper = new Swiper(".swiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+
+document.querySelectorAll('.sukai').forEach(function (icon) {
+    icon.addEventListener('mouseover', function () {
+        this.classList.replace('ri-heart-line', 'ri-heart-fill');
+    });
+    icon.addEventListener('mouseout', function () {
+        this.classList.replace('ri-heart-fill', 'ri-heart-line');
+    });
+});
+
+document.querySelectorAll('.keranjangi').forEach(function (icon) {
+    icon.addEventListener('mouseover', function () {
+        this.classList.replace('ri-shopping-cart-2-line', 'ri-shopping-cart-fill');
+    });
+    icon.addEventListener('mouseout', function () {
+        this.classList.replace('ri-shopping-cart-fill', 'ri-shopping-cart-2-line');
+    });
+});
