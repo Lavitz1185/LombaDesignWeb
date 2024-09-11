bba14da1a2083640db5a34c289a262eae2504847
AdiJT <aditaklal@gmail.com>
2024-09-11 19:45:35 +0800

    Sedikit

---
+++ OlehOlehNTT/OlehOlehNTT.Web/Components/RemixIcons.cs
@@ -0,0 +1,15 @@
+﻿namespace OlehOlehNTT.Web.Components;
+
+public static class RemixIcons
+{
+    public const string Close = "close";
+    public const string Menu = "menu";
+    public const string ArrowRightUp = "arrow-right-up";
+    public const string Heart = "heart";
+    public const string ShoppingCart2 = "shopping-cart-2";
+    public const string Grid = "grid";
+    public const string MapPin = "map-pin";
+    public const string Instagram = "instagram";
+    public const string Facebook = "facebook";
+    public const string Twitter = "twitter";
+}
